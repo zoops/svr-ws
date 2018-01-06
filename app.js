0211210f42684a1b2443bdbb2d20ea7348a38f05
zoops <jhbaik@moberan.com>
2018-01-06 20:44:13 +0900

    t1

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express        = require( 'express' );
 var http           = require( 'http' );
 var app            = express();
-var expressWs      = require('express-ws')(app);
+// var expressWs      = require('express-ws')(app);
 
-//*
 app.set( 'port', process.env.PORT || 3001 );
+/*
 app.get('/', function (req, res) {
     res.send('Hello World');
 });
@@ -12,25 +12,26 @@ http.createServer( app ).listen( app.get( 'port' ), function (){
   console.log( 'Express server listening on port ' + app.get( 'port' ));
 });
 //*/
+//*
 /*
 app.use(function (req, res, next) {
   console.log('middleware');
   req.testing = 'testing';
   return next();
 });
- 
+ */
 app.get('/', function(req, res, next){
-  console.log('get route', req.testing);
-
   res.send('Hello World');  
   //  res.end();
 });
-
+/*
 app.ws('/echo', function(ws, req) {
   ws.on('message', function(msg) {
     ws.send(msg);
   });
 });
- 
-app.listen(3000);
 */
+app.listen(app.get( 'port' ), function(){
+  console.log( 'Express server listening on port ' + app.get( 'port' ));
+});
+//*/
