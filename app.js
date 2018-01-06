7dccaea764a74b6607f491b4bf9335c97e5648c6
zoops <jhbaik@moberan.com>
2018-01-06 20:46:48 +0900

    t3

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ app.get('/', function(req, res, next){
   res.send('Hello World');  
   //  res.end();
 });
-/*
+
 app.ws('/echo', function(ws, req) {
   ws.on('message', function(msg) {
     ws.send(msg);
   });
 });
-*/
+
 app.listen(app.get( 'port' ), function(){
   console.log( 'Express server listening on port ' + app.get( 'port' ));
 });
